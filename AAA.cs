86d4f719c0c98f192d0e350ee190c637250c8063
Syooian <msn0126@hotmail.com>
2025-04-07 16:24:19 +0800

    備份

---
+++ AAA.cs
@@ -0,0 +1,22 @@
+var Num = 95;
+
+switch (Num / 10)
+{
+    case 10:
+    case 9:
+        document.write("A<br>");
+        break;
+    case 8:
+        document.write("B<br>");
+        break;  case Num === 3:
+                document.write("C<br>");
+                break;
+            case Num === 4:
+                document.write("D<br>");
+                break;
+            case Num === 5:
+                document.write("E<br>");
+                break;
+            default:
+                document.write("default<br>");
+        }
