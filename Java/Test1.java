cdf97a6792d72a407aa827a7f87a3f9171d39c52
Syooian <msn0126@hotmail.com>
2025-04-17 15:22:10 +0800

    備份

---
+++ Java/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println(args.length);
+
+        
+    }
+}
